50c621385588d10868ee5660f0bfb15f68e42730
Jason Holm <46735884+JasonHolm@users.noreply.github.com>
2020-08-06 17:27:08 -0700

    updated basic graphs and added a couple new ones

--- game_analysis.py
+++ game_analysis.py
@@ -1,49 +1,90 @@
 import sys
+from enum import Enum
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+WANTED_PLATFORMS = ["NES", "SNES", "N64", "GC", "Wii", "WiiU", "NS", "PS", "PS2", "PS3", "PS4", "XB", "X360", "XOne"]
+PLATFORM_COLORS = ["blue", "green", "blue", "red", "blue", "blue", "green", "green", "red", "red", "red", "red", "red", "red"]
+
+class Column(Enum):
+    YEAR = "Year"
+    CRITIC = "Critic_Score"
+    USER = "User_Score"
+    PUBLISHER = "Publisher"
+    PLATFORM = "Platform"
 
 class Analysis(object):
 
     # Initialize the pandas DataFrames.
     def __init__(self, game_file):
         self.games = pd.read_csv(game_file)
-        self.years_dict = {}
-        self.critic_dict = {}
+        self.simple_dicts = []
+        self.rating_dicts = []
 
     def create_dataframe(self, col_name):
+        col_dict = {}
         df = pd.DataFrame(self.games, columns=[col_name, "Total_Shipped"])
         col = df[col_name].unique()
-        col = np.delete(col, np.where(np.isnan(col)))
-        # print(col)
+        if col_name == "Critic_Score" or col_name == "User_Score":
+            col = np.delete(col, np.where(np.isnan(col)))
         for value in col:
-            if col_name == "Year":
-                self.years_dict[value] = df.loc[df["Year"] == value, "Total_Shipped"].sum()
-                # print(self.years_dict)
-            elif col_name == "Critic_Score":
-                self.critic_dict[value] = df.loc[df["Critic_Score"] == value, "Total_Shipped"].sum()
-                # print(self.critic_dict)
+            sum = df.loc[df[col_name] == value, "Total_Shipped"].sum()
+            if col_name == "Publisher" or col_name == "Platform":
+                if col_name == "Platform":
+                    if value in WANTED_PLATFORMS:
+                        col_dict[value] = sum
+                else:
+                    if sum > 200:
+                        col_dict[value] = sum
+            else:
+                col_dict[value] = sum
+        self.simple_dicts.append(col_dict)
 
-    def plot_dataframe(self, col_name):
-        if col_name == "Year":
-            plt.bar(list(self.years_dict.keys()), list(self.years_dict.values()))
-            plt.xlabel("Year")
-            plt.ylabel("Total Sales")
-        elif col_name == "Critic_Score":
-            plt.bar(list(self.critic_dict.keys()), list(self.critic_dict.values()))
-            plt.xlabel("Critic Score")
-            plt.ylabel("Total Sales")
+    def plot_dataframe(self, col_name, i):
+        l = sorted(self.simple_dicts[i].items(), key=lambda e: e[1], reverse=True)
+        keys = []
+        values = []
+        for (k,v) in l:
+            keys.append(k)
+            values.append(v)
+        if col_name == "Platform":
+            plt.bar(keys, values, color=PLATFORM_COLORS)
+        elif col_name == "Publisher":
+            plt.barh(keys, values)
+        else:
+            plt.bar(keys, values)
+        plt.xlabel(col_name)
+        plt.ylabel("Total Game Sales")
         plt.show()
+
+    def create_and_plot_ratings(self):
+        for col_name in ["Critic_Score", "User_Score"]:
+            col_dict = {}
+            df = pd.DataFrame(self.games, columns=[col_name, "Year"])
+            col = df["Year"].unique()
+            for value in col:
+                sum = df.loc[df["Year"] == value, col_name].sum()
+                count = df.loc[df["Year"] == value, col_name].count()
+                average = sum / count
+                col_dict[value] = average
+            self.rating_dicts.append(col_dict)
+            plt.plot(list(col_dict.keys()), list(col_dict.values()))
+            plt.xlabel("Year")
+            plt.ylabel(col_name)
+            plt.show()
 
 def main():
     games_file = sys.argv[1]
 
     a = Analysis(games_file)
-    a.create_dataframe("Year")
-    a.create_dataframe("Critic_Score")
-    a.plot_dataframe("Year")
-    a.plot_dataframe("Critic_Score")
+    #for i, column in enumerate(Column):
+        #print(column.value)
+        #a.create_dataframe(column.value)
+        #a.plot_dataframe(column.value, i)
+    a.create_and_plot_ratings()
+
 
 if __name__ == "__main__":
     main()
